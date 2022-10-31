ad735409ad6d2b3af3778e41b2889b9574ca263b
beyazleader <116661596+beyazleader@users.noreply.github.com>
2022-10-31 11:32:14 +0300

    Delete proje1.java

--- proje1.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-public class proje1 {
-    public static void main(String[] args){
-        int mat,türkce,spor,sanat,tarih,secemeliders ;
-        Scanner inp=new Scanner(System.in);
-    }
-}
